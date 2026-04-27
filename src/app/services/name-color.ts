537f1ed7c2e4554fc1add0d2a1ea79b5c011293c
Benedict Fels <benedictfels@web.de>
2026-04-27 17:13:25 +0200

    grouped contact by first letter

--- src/app/services/name-color.ts
+++ src/app/services/name-color.ts
@@ -8,13 +8,14 @@ export class NameColor {
 
   palette: string[] = [
     '#2E5BFF', '#D7263D', '#1B998B', '#F46036',
-    '#662E9B', '#202C39', '#C1292E', '#44803F',
-    '#54478C', '#048BA8', '#A4036F', '#2F2F2F',
-    '#E5446D', '#FF9F1C', '#5E2BFF', '#006466'
+    '#662E9B', '#014b11', '#C1292E', '#44803F',
+    '#54478C', '#048BA8', '#A4036F', '#ffe600',
+    '#E5446D', '#FF9F1C', '#5E2BFF', '#006466',
+    '#7B3F00', '#4A5859', '#916953', '#310D20'
   ];
 
   getInitials(firstname: string, name: string): string {
-    let fullName = name + " " + firstname;
+    let fullName = firstname + " " + name;
     return fullName
       .split(' ')
       .map(n => n[0])
@@ -26,10 +27,12 @@ export class NameColor {
   getColor(uid: number): string {
     console.log(uid);
 
-    let colorNumber = uid % 16;
+    let colorNumber = uid % this.palette.length;
 
     console.log(this.palette[colorNumber]);
 
     return this.palette[colorNumber];
   }
+
+  
 }

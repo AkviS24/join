2e96d88877e2147c4df67aa50ea36ab9644cb5cf
Akvis24 <akvis2022@gmail.com>
2026-04-30 14:14:44 +0200

    Subscription for all changes implemented

--- src/app/services/database.ts
+++ src/app/services/database.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { createClient } from '@supabase/supabase-js';
+import { createClient, RealtimeChannel } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +10,7 @@ export class Database {
   supabase = createClient(this.supabaseUrl, this.supabaseKey);
 
   demoDaten = signal<{ id: number, created_at: string, name: string, email: string, password: string, phone: number, isLoggedIn: boolean }[]>([]);
+  channels: RealtimeChannel | undefined;
 
   async getData() {
     let { data: contacts, error } = await this.supabase
@@ -18,6 +19,20 @@ export class Database {
       .order('name', { ascending: true });
     if (!contacts) return
     this.demoDaten.set(contacts)
+
+    this.channels = this.supabase.channel('custom-all-channel')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'contacts' },
+        (payload) => {
+          console.log('Change received!', payload)
+        }
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy() {
+    this.supabase.removeChannel(this.channels!);
   }
 
   async setData(demoData: { name: string, email: string, password: string, phone: number }) {
@@ -36,7 +51,7 @@ export class Database {
   }
 
   async deleteData(id: number) {
-    const {data, error} = await this.supabase
+    const { data, error } = await this.supabase
       .from('contacts')
       .delete()
       .select()

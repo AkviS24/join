27376779c9841e4160fa01d08ef49b117851ce1d
Akvis24 <akvis2022@gmail.com>
2026-05-04 10:18:13 +0200

    test

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -8,7 +8,7 @@ export class Supabase {
   supabaseUrl = 'https://ihqvvagcuemrsbalsksp.supabase.co';
   supabaseKey = 'sb_publishable_N4wmb3jqA8vxuofrj9kFPg_45-BAgZo';
   supabase = createClient(this.supabaseUrl, this.supabaseKey);
-  channels: RealtimeChannel | undefined;
+  // channels: RealtimeChannel | undefined;
 
   demoDaten = signal<{ id: number, created_at: string, name: string, email: string, phone: number, loggedIn: boolean, password: string }[]>([]);
 
@@ -21,20 +21,20 @@ export class Supabase {
     if (!demoDB) return
     this.demoDaten.set(demoDB)
 
-    this.channels = this.supabase.channel('custom-all-channel')
-      .on(
-        'postgres_changes',
-        { event: '*', schema: 'public', table: 'contacts' },
-        (payload) => {
-          console.log('Change received!', payload)
-        }
-      )
-      .subscribe()
+    // this.channels = this.supabase.channel('custom-all-channel')
+    //   .on(
+    //     'postgres_changes',
+    //     { event: '*', schema: 'public', table: 'contacts' },
+    //     (payload) => {
+    //       console.log('Change received!', payload)
+    //     }
+    //   )
+    //   .subscribe()
   }
 
-  ngOnDestroy() {
-    this.supabase.removeChannel(this.channels!);
-  }
+  // ngOnDestroy() {
+  //   this.supabase.removeChannel(this.channels!);
+  // }
 
   async setDemoData(demoData: { name: string, email: string, phone: number, password: string }) {
     const { data, error } = await this.supabase

79fb1aa2b05a171346aa4c8de51337156ad5b006
Akvis24 <akvis2022@gmail.com>
2026-05-05 10:27:57 +0200

    environments update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    supabaseUrl: 'https://ihqvvagcuemrsbalsksp.supabase.co',
+    supabaseKey: 'sb_publishable_N4wmb3jqA8vxuofrj9kFPg_45-BAgZo',
+};
